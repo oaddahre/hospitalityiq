8d955f77fb0c47c03df7eca43d204c32fdc47480
Othmane Addahre <othmaneaddahre@Othmanes-MacBook-Air.local>
2026-06-01 11:09:35 +0100

    Switch map tiles to CartoDB Positron light theme

--- static/js/main.js
+++ static/js/main.js
@@ -351,7 +351,7 @@ function popupHTML(h) {
 function initMap() {
   const map = L.map('map-container', { zoomControl: true }).setView(MOROCCO_CENTER, MOROCCO_ZOOM);
 
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 18,
